15d72e4e4ea4c3d5fbeff4860400eab2a013530f
chau1705 <86762088+chau1705@users.noreply.github.com>
2021-07-01 12:25:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bae à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim Chị lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Trần Đỗ Quấ Trâm nhấn vô đâyyy',
     title: 'Phải chăng em đã yêu Chị ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say Chị ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích chị <33',
